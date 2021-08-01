87e9d4ef6af5e8688314e10f50890f0c19d9e02c
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-01 23:17:11 +0800

    Update calculator.js

--- 01-Fundamentals-Part-1/calculator.js
+++ 01-Fundamentals-Part-1/calculator.js
@@ -1,21 +1,21 @@
-var operationButtons = document.getElementsByClassName('operation-button');
+let operationButtons = document.getElementsByClassName('operation-button');
 
-var input1 = document.getElementById('number1');
-var input2 = document.getElementById('number2');
+const input1 = document.getElementById('number1');
+const input2 = document.getElementById('number2');
 
 function makeOperation(operationCode) {
 
-	var number1 = Number(input1.value);
-	var number2 = Number(input2.value);
+	const number1 = Number(input1.value);
+	const number2 = Number(input2.value);
 
 		if (operationCode === '+') {
-			var result = number1 + number2;
+			const result = number1 + number2;
 		} else if (operationCode === '-') {
-			var result = number1 - number2;
+			const result = number1 - number2;
 		} else if (operationCode === '*') {
-			var result = number1 * number2;
+			const result = number1 * number2;
 		} else if (operationCode === '/') {
-			var result = number1 / number2;
+			const result = number1 / number2;
 		} else {
 			window.alert('operation is unknown');
 		}
@@ -23,12 +23,12 @@ function makeOperation(operationCode) {
 }
 
 function onOperationButtonClick(eventObject) {
-	var clickedElement = eventObject.currentTarget;
-	var operation = clickedElement.innerHTML
+	const clickedElement = eventObject.currentTarget;
+	const operation = clickedElement.innerHTML
 	makeOperation(operation);
 }
 
-for(var i = 0; i < operationButtons.length; i++) {
-	var button = operationButtons[i];
+for(let i = 0; i < operationButtons.length; i++) {
+	const button = operationButtons[i];
 	button.addEventListener('click', onOperationButtonClick); 
 }
