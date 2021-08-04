0e4158a4f9e2a1e7bac305f921b4895e343479f6
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-05 00:15:04 +0800

    Update MathActionList.js

--- 01-Fundamentals-Part-1/MathActionList.js
+++ 01-Fundamentals-Part-1/MathActionList.js
@@ -1,3 +1,9 @@
 // Popular Math action(function): 
 
 Math.pow(2, 2); // => 4 
+Math.round(.6); // => 1 (Rounding to the nearest integer)
+Math.ceil(.6); // => 1
+Math.floor(.6); // => 0
+Math.abs(-5); // => 5 (Absolute value)
+Math.max(x,y,z); // return MAX argument
+Math.min(x,y,z); // return MIN argument
