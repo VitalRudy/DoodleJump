b8d7a3e05387f6661c62bda0c6506df00b0b683f
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-09 21:08:28 +0800

    Update stringOperations.js

--- 01-Fundamentals-Part-1/stringOperations.js
+++ 01-Fundamentals-Part-1/stringOperations.js
@@ -7,4 +7,36 @@ string.slice(-3);  // => "ing" The last three symbols.
 string.split(", ");  // => [ "This is a new string" ]
 
 //Searching in a string:
+string.indexOf("i"); // => 2, position of i letter.
+string.indexOf("s"); // => 3.
+string.indexOf("mm"); // => -1 NOT INCLUDING.
+string.lastIndexOf("i"); // => 17;
+
+// Boolean function searching ( start from ES6 ):
+string.startsWith("Thi"); // => TRUE
+string.endsWith("!"); // => FALSE
+string.includes("ing"); // => TRUE
+
+//Creating a modification string's version:
+string.replace("his", "ose"); // => 
+string.toLowerCase(); // smaler characters
+string.toUpperCase(); // bigger characters
+string.normalize();
+
+//operations with a characters:
+string.charAt(0); // => 'T'
+string.charAt(string.length-1); // => 'g' the last character
+string.charCodeAt(); // => 84; 16 - bits numbers.
+string.charPointAt(); // 
+
+//Adding string function (ES2017) :
+"x".padStart(3) // Adding left spaces
+"x".padEnd(3) // Adding right spaces
+"x".padStart(3, "*");
+"x".padEnd(3, "-");
+
+//Mixed string methods:
+string.concat("!"); // 'This is a new string!'
+"<>".repeat(5); // "<><><><><>"
+
 
