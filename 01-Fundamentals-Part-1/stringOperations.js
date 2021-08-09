c7cbe1bb25c747aceb95ac77e3e2c75478606ed7
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-09 20:14:26 +0800

    Create stringOperations.js

---
+++ 01-Fundamentals-Part-1/stringOperations.js
@@ -0,0 +1,7 @@
+let string = "This is a new string";
+
+// Getting a part of a string:
+string.substring(1,4);  // =>
+string.slice(1,4);  // =>
+string.slice(-3);  // =>
+string.split(", ");  // =>
