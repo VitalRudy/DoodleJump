cab817de085fd5ee70af7bfe4abb21f48a42c86a
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-14 21:24:30 +0800

    Update tasks.js

--- 01-Fundamentals-Part-1/tasks.js
+++ 01-Fundamentals-Part-1/tasks.js
@@ -6,10 +6,10 @@ const a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
 // Solution 1 
 
-const f1 = () = > {
+const f1 = () => {
   let out = '';
   for(let i = 0; i < a.length; i++) {
-    if (a[i] < 5) out a[i] + ' ';
+    if (a[i] < 5) out += a[i] + ' ';
   }
   console.log(out);
 }
