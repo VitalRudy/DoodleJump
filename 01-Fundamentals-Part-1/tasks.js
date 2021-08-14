0e8099a777586aa200db593a4e84a8215925a5c8
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-14 21:19:54 +0800

    Create file : tasks.js

---
+++ 01-Fundamentals-Part-1/tasks.js
@@ -0,0 +1,17 @@
+// Task 1
+// There is a (Array) = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+// Output all elements, that < 5,
+
+const a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+// Solution 1 
+
+const f1 = () = > {
+  let out = '';
+  for(let i = 0; i < a.length; i++) {
+    if (a[i] < 5) out a[i] + ' ';
+  }
+  console.log(out);
+}
+
+f1();
