b47585fa258532575ea9eaf7caaa293f08f29d42
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-19 23:24:54 +0800

    Create destructingAssignment.js

---
+++ 01-Fundamentals-Part-1/destructingAssignment.js
@@ -0,0 +1,11 @@
+//Example for Destructing Assignment
+
+let [x,y] = [1,2]; // => let x = 1, y = 2; 
+[x,y] = [x+1, y+1]; // => x = x + 1; y = y + 1;
+[x, y] = [y, x]; // replace x and y;
+[x, y]; // => [3,2]
+
+
+
+
+
