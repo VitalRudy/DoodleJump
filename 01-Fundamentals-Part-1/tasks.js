08d6669f42dc4b0dab0775db29810ad66ea47454
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-08-22 22:59:31 +0800

    Update tasks.js

--- 01-Fundamentals-Part-1/tasks.js
+++ 01-Fundamentals-Part-1/tasks.js
@@ -226,4 +226,25 @@ const input3 = [4,9,5],
 const input4 = [9,4,9,8,4];
 //Output: [4,9] or [9,4]
 
+const intersection = function (nums1, nums2) {
+  let result = [];
+  
+  let map = nums1.reduce((acc, i) => {
+    acc[i] = acc[i] ? acc[i] + 1: 1;
+    return acc;
+  }, {});
+  
+  for (let i = 0; i < nums2.length; i++) {
+    const current = nums2[i];
+    let count = map[current];
+    
+    if (count && count > 0) {
+      reult.push(current);
+      map[current] -= 1;
+  }
+}
+  return result;
+};
 
+console.log(intersetion(input1, input2));
+console.log(intersetion(input3, input4));
