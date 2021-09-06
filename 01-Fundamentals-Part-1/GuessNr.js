924e9b0c63b8d4e2b662831e629e522331b8ba59
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-09-06 20:40:56 +0800

    Delete GuessNr.js

--- 01-Fundamentals-Part-1/GuessNr.js
+++
@@ -1,6 +0,0 @@
-// Guessing Number Game
-
-//Generate a random number +
-//Give the user the abillity to guess
-//If they guess and they are wrong, ask them again(hint);
-//If they win - say that they won.
