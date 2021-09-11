d5e8a3aa1a5774034752a94622fe208fc3afd317
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-09-11 23:48:48 +0800

    Update script.js

--- Guessing Nr Game/script.js
+++ Guessing Nr Game/script.js
@@ -2,3 +2,8 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 11;
+document.querySelector('.score').textContent = 3;
+
+document.querySelector('.guess').value = 22;
+console.log(document.querySelector('.guess').value);
