b2119a76bbb2c04eb531e440b1a923e0d29b6316
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-09-12 23:12:37 +0800

    Create script.js

--- Guessing Nr Game/script.js
+++ Guessing Nr Game/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 11;
@@ -7,3 +8,4 @@ document.querySelector('.score').textContent = 3;
 
 document.querySelector('.guess').value = 22;
 console.log(document.querySelector('.guess').value);
+*/
