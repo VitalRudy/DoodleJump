8361b963de29cf0bf486bb4873dfa9ba5e496bbc
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-09-18 00:22:18 +0800

    Update script.js

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
