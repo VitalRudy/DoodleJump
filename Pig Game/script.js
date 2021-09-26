a52093041b2fde3dfb4c6c691704dc4dd7864bbc
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-09-27 00:00:03 +0800

    Update script.js

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,7 +1,12 @@
 'use strict';
 
 // Selecting elements
-
+const player1El = document.querySelector('.player--0');
+const player0El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
